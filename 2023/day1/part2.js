c7ae37024789a822fbaa6bcafba768f915120e2a
Kristiyan Ivanov <fddfddfddfddfddfdd@gmail.com>
2024-07-23 11:15:18 +0300

    idk

--- 2023/day1/part2.js
+++ 2023/day1/part2.js
@@ -3,6 +3,7 @@ const readline = require('readline')
 
 const path = './input.txt'
 const fileStream = fs.createReadStream(path)
+const digits = ['one','two','three','four','five','six','seven','eight','nine']
 let sum = 0
 
 const rl = readline.createInterface({
@@ -12,30 +13,37 @@ const rl = readline.createInterface({
 
 rl.on('line', line => {
     let first, last
+
+    for (let i in digits) {
+        if (line.includes(digits[i])) {
+            console.log(line.includes(digits[i]), line, digits[i])
+        }
+    }
+
     for (let x in line) {
-       if (/[0-9]/.test(line[x])) {
-           if (!first) {
-               first = line[x]
-           }
-           else {
-               last = line[x]
-           }
-       }
+       //if (/[0-9]/.test(line[x])) {
+       //    if (!first) {
+       //        first = line[x]
+       //    }
+       //    else {
+       //        last = line[x]
+       //    }
+       //}        
     }
 
-    if (!last) {
-        last = first
-    }
+    //if (!last) {
+    //    last = first
+    //}
 
-    const two_digit_num = first + last
-    
-    if (two_digit_num) {
-        sum += Number(two_digit_num)
-    }
+    //const two_digit_num = first + last
 
-    console.log(`line: ${line}, num: ${two_digit_num}, sum: ${sum}`)
+    //if (two_digit_num) {
+    //    sum += Number(two_digit_num)
+    //}
+
+    //console.log(`line: ${line}, num: ${two_digit_num}, sum: ${sum}`)
 })
 
 rl.on('close', () => {
-    console.log(`sum: ${sum}`)
+    //console.log(`sum: ${sum}`)
 })
