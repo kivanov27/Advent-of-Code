a89ced3d08ac605155d33a242f56cb503aee9e58
fdd <kristiyan.ivanov.98@gmail.com>
2024-12-21 13:29:14 +0200

    day3 part2 done and day4 part1 done

--- 2024/day3/script.py
+++ 2024/day3/script.py
@@ -2,14 +2,21 @@ import re
 
 total = 0
 matches = []
-pattern = r'mul\(\d{1,3},\d{1,3}\)'
+pattern = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
+do = True
 
 with(open('input.txt', 'r') as file):
     for line in file:
         matches.extend(re.findall(pattern, line))
 
 for match in matches:
-    nums = re.findall(r'\d{1,3}', match)
-    total += int(nums[0]) * int(nums[1])
+    if match == 'do()':
+        do = True
+    elif match == "don't()":
+        do = False
+    elif re.match(r'mul\(\d{1,3},\d{1,3}\)', match):
+        if do:
+            nums = re.findall(r'\d{1,3}', match)
+            total += int(nums[0]) * int(nums[1])
 
 print(total)
---
+++ 2024/day4/script.py
@@ -0,0 +1,38 @@
+matrix = []
+count = 0
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        matrix.append(list(line.strip()))
+
+for col in range(len(matrix)):
+    for row in range(len(matrix[0])):
+
+        # match horizontal and reverse
+        if row < len(matrix[0]) - 3:
+            word = matrix[col][row] + matrix[col][row+1] + matrix[col][row+2] + matrix[col][row+3]
+            if word == 'XMAS' or word[::-1] == 'XMAS':
+                count += 1
+
+        # match vertical and reverse
+        if col < len(matrix) - 3:
+            word = matrix[col][row] + matrix[col+1][row] + matrix[col+2][row] + matrix[col+3][row]
+            if word == 'XMAS' or word[::-1] == 'XMAS':
+                count += 1
+
+        # match l-r diagonal and reverse
+        if col < len(matrix) - 3 and row < len(matrix[0]) - 3:
+            word = matrix[col][row] + matrix[col+1][row+1] + matrix[col+2][row+2] + matrix[col+3][row+3]
+            if word == 'XMAS' or word[::-1] == 'XMAS':
+                count += 1
+
+        # match r-l diagonal and reverse
+        if col < len(matrix) - 3 and row < len(matrix[0]) - 3:
+            word = matrix[col][row+3] + matrix[col+1][row+2] + matrix[col+2][row+1] + matrix[col+3][row]
+            if word == 'XMAS' or word[::-1] == 'XMAS':
+                count += 1
+
+        # check horizontal for last 3 columns
+        
+
+print(count)
