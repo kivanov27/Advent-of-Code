6caae81ba3c78866103a19834fa864f1e2c56add
fdd <kristiyan.ivanov.98@gmail.com>
2025-06-09 17:04:38 +0300

    done with day1, both parts

--- 2017_cpp/day1/main.cpp
+++ 2017_cpp/day1/main.cpp
@@ -5,22 +5,24 @@ int main() {
     std::ifstream inputFile("input.txt");
     if (inputFile) {
         inputFile.seekg(0, std::ios::end);
-        int size = inputFile.tellg();
+        int size = (int)inputFile.tellg() - 1;
+
+        std::cout << "Size: " << size << '\n';
 
         inputFile.seekg(0);
-        char* input = new char[size];
+        char* input = new char[size + 1];
         inputFile.read(input, size);
 
         int sum = 0;
 
-        for (int i = 0; i < size - 1; ++i) {
-            int j = (i + ((size - 1) / 2)) % size;
-            std::cout << "i: " << i << "   input[i]: " << input[i] << '\n';
-            std::cout << "j: " << j << "   input[j]: " << input[j] << '\n';
+        for (int i = 0; i < size; ++i) {
+            int j = (i + (size / 2)) % size;
+            // std::cout << "i: " << i << "   input[i]: " << input[i] << '\n';
+            // std::cout << "j: " << j << "   input[j]: " << input[j] << '\n';
             if (input[i] == input[j]) {
-                std::cout << "Add i: " << input[i] << '\n';
+                // std::cout << "Add i: " << input[i] << '\n';
                 sum += input[i] - '0';
-                std::cout << "Sum: " << sum << '\n';
+                // std::cout << "Sum: " << sum << '\n';
             }
         }
 
