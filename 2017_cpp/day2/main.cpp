951a0a69a7d6091673e5c6880835627750916ade
fdd <kristiyan.ivanov.98@gmail.com>
2025-06-09 18:54:33 +0300

    2017 C++, Day 2, Parts 1 and 2 done.

--- 2017_cpp/day2/main.cpp
+++ 2017_cpp/day2/main.cpp
@@ -2,39 +2,35 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 int main() {
     std::ifstream file("input.txt");
     std::string line;
-    int sum;
+    int sum = 0;
 
     if (file.is_open()) {
         while (std::getline(file, line)) {
             std::istringstream iss(line);
             int x;
-            int lowest {};
-            int highest {};
-            bool isLowestSet = false;
+            std::vector<int> numbers;
 
             while (iss >> x) {
-                if (!isLowestSet) {
-                    lowest = x;
-                    isLowestSet = true;
-                }
-
-                if (x < lowest) {
-                    lowest = x;
-                }
-                else if (x > highest) {
-                    highest = x;
-                }
+                numbers.push_back(x);
             }
 
-            sum += highest - lowest;
-            std::cout 
-                << "Lowest: " << lowest 
-                << " | Highest: " << highest 
-                << " | Sum: " << sum << '\n';
+            for (int i = 0; i < numbers.size() - 1; ++i) {
+                for (int j = i + 1; j < numbers.size(); ++j) {
+                    if (numbers[i] % numbers[j] == 0) {
+                        sum += numbers[i] / numbers[j];
+                        break;
+                    }
+                    else if (numbers[j] % numbers[i] == 0) {
+                        sum += numbers[j] / numbers[i];
+                        break;
+                    }
+                }
+            }
         }
     }
     else {
