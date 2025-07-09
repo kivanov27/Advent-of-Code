de2ff6755e8eaf377e5cca8fb65336a541527aa3
fdd <kristiyan.ivanov.98@gmail.com>
2025-07-09 11:19:12 +0300

    starting part2

---
+++ 2015_py_cpp/day8/part2.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+    std::ifstream file("input.txt");
+    if (file) {
+        std::string line;
+        std::string newLine = "\"";
+        unsigned diff = 0;
+
+        while (std::getline(file, line)) {
+            for (int i = 0; i < line.length(); ++i) {
+                if (line[i] == '\\') {
+                    newLine.push_back('\\');
+                }
+                else if (line[i] == '\"') {
+                    newLine.push_back('\"');
+                }
+                newLine.push_back(line[i]);
+            }
+
+            newLine.push_back('\"');
+
+            unsigned codeLen = line.length();
+            unsigned memLen = newLine.length();
+
+            // debug
+            std::cout << "Line: " << line << '\n';
+            std::cout << "New line: " << newLine << '\n';
+            // std::cout << "codeLen: " << codeLen << '\n';
+            // std::cout << "memLen: " << memLen << '\n';
+            std::cout << '\n';
+        }
+
+        // std::cout << diff << '\n';
+    } else {
+        std::cerr << "Couldn't open input file" << '\n';
+    }
+
+    file.close();
+    return 0;
+}
