148f3191dc0402cb411df7083124f94524d6b6cc
fdd <kristiyan.ivanov.98@gmail.com>
2026-01-28 15:45:41 +0200

    Completed day 5 part 2!

---
+++ 2025/day5/part2.py
@@ -0,0 +1,22 @@
+import re
+
+ranges = []
+
+with open("input.txt", 'r') as file:
+    for line in file:
+        line = line.strip()
+        if not line:
+            break
+        x, y = map(int, re.findall(r'\d+', line))
+        ranges.append([x, y])
+
+ranges.sort()
+merged = []
+
+for start, end in ranges:
+    if not merged or start > merged[-1][1]:
+        merged.append([start, end])
+    else:
+        merged[-1][1] = max(end, merged[-1][1])
+
+print(sum(end - start + 1 for start, end in merged))
